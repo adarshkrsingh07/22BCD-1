20f4bc52ce3cbc824a86fb05ea20a699757770d8
adarshkrsingh07 <adarshkrdixit@gmail.com>
2024-04-03 12:01:41 +0530

    Exp7.c commited for adding 2 nos

---
+++ Exp7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5; 
+    int num2 = 7; 
+    int sum;
+    sum = num1 + num2;
+
+    printf("Sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
+
